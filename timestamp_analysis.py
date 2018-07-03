44335cefd7b9db309ed62f649d4c6f869cac362f
AnkilP <a293pate@uwaterloo.ca>
2018-07-03 10:57:40 -0400

    greedy algorithm for index matching

--- timestamp_analysis.py
+++ timestamp_analysis.py
@@ -4,39 +4,47 @@ import os
 import time
 import numpy as np
 from os.path import isfile, join
+import pandas as pd
 
-class compare_timestamps(object):
-    def __init__(self, file_path_to_timestamps,fps, dataframe_camera, dataframe_velodyne, *args):
-        self.file_path_to_timestamps = file_path_to_timestamps
-        self.__image = __image
-        self.fps = fps
-        self.dataframe_camera = dataframe_camera
-		self.dataframe_velodyne = dataframe_velodyne
-       
-    def convert_frames_to_video(self, pathIn,pathOut,fps):
-        frame_array = []
-        files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
- 
-        #for sorting the file names properly
-        files.sort(key = lambda x: int(x[5:-4]))
- 
-        for i in range(len(files)):
-            filename = pathIn + files[i]
-            #reading each files
-            img = cv2.imread(filename)
-            height, width, layers = img.shape
-            size = (width,height)
-            print(filename)
-            #inserting the frames into an image array
-            frame_array.append(img)
- 
-        out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
- 
-        for i in range(len(frame_array)):
-            # writing to a image array
-            out.write(frame_array[i])
-        out.release() 
+class timestamps(object):
 
-    def time_stamp_parsing(self, time_stamp_file_path, dataframe_camera, dataframe2):
-		dataframe_camera = pd.read_table(time_stamp_file_path)
-		df['date'], df['timestamps'] = df['2011-09-26 13:02:25.445761280'].str.split(' ', 1).str
+	fps = 25
+
+    def __init__(self, pathIn, *args):
+        self.pathIn = pathIn
+		self.values
+
+	def time_stamp_parsing(self, time_stamp_file_path, df):
+		df = pd.read_table(time_stamp_file_path, header= None)
+		df['date'], df['timestamps'] = df[0].str.split(' ', 1).str
+		df = df.drop(0, 1)
+		self.values = pd.to_timedelta(df['timestamps'], unit = 's')
+		return 0
+
+class time_stamp_pairs(timestamps):
+
+
+
+class timestamps_comparison(time_stamp_pairs):
+
+	def __init__(self, *args):
+		#stuff I need
+
+	def greedy_algorithm(self, timestamps1, timestamps2, *args):
+		index_matched_points = []
+		i = 0
+		#for i in range(len(timestamps_camera)): #slower one on top
+		while i < len(timestamps_camera):
+			#for j in range(timestamps_camera): #faster one at the bottom
+			indexe = [0,0]
+			j = 0
+        
+			while(timestamps_velodyne[j] >= timestamps_camera[i]):
+				indexe[0] = timestamps_velodyne[i]
+				indexe[1] = timestamps_camera[j]
+				i = i + 1
+				print("lets see what happens now")
+			j = j+1
+			index_matched_points.append(indexe)
+			#indexe = [0,0]
+		return index_matched_points
